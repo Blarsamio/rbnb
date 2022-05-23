9e165a71c819d475209763a96c785733884beba6
Patricio Perez Herrero <patriciopherrero@gmail.com>
2022-05-23 12:46:31 +0200

    Managed all the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :devs
+  resources :devs, only: %i[create index new show] do
+    resources :bookings, only: :create
+  end
+  resources :bookings, only: %i[index destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
