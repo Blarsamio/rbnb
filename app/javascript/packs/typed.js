e816e3ef6c22ddca1e88d10ddbbe21ff262c2ac5
Patricio Perez Herrero <patriciopherrero@gmail.com>
2022-05-26 16:16:41 +0200

    Dev show page finished

--- app/javascript/packs/typed.js
+++ app/javascript/packs/typed.js
@@ -1,20 +1,38 @@
 import Typed from 'typed.js';
 
-var options = {
-  strings: [' start today and find the best dev for your project', 'browse between everyone we have in stock'],
-  typeSpeed: 100
-};
-
-var typed = new Typed('.element', options);
-
-// var options2 = {
-//   strings: ["Web Developer, expert in: ${@developer.name}"],
+// var options = {
+//   strings: [' start today and find the best dev for your project', 'browse between everyone we have in stock'],
 //   typeSpeed: 100
 // };
 
-// var typed2 = new Typed('.element2', options2);
+// var typed = new Typed('.element', options);
 
-// // Web Developer, expert in: @developer.language
-// //               <p class="font-italic mb-0">Lives in: <%= @developer.address %></p>
-// //               <p class="font-italic mb-0"><%= @developer.user.email %></p>
-// //               <p class="font-italic mb-0"><%= @developer.description
+const element = document.querySelector(".element")
+
+if (element) {
+  var options = {
+    strings: [' start today and find the best dev for your project', 'browse between everyone we have in stock'],
+    typeSpeed: 100
+  };
+
+  var typed = new Typed('.element', options);
+}
+
+const element2 = document.querySelectorAll(".element2")
+
+if (element2) {
+  const details = document.querySelectorAll(".about-main p")
+  const strings = [details[0].innerText, details[1].innerText, details[2].innerText, details[3].innerText]
+  var options2 = {
+    strings: ['this is me', 'these are my details'],
+    typeSpeed: 100,
+    smartBackspace: false
+  };
+  var typed2 = new Typed('.element2', options2);
+}
+
+
+// Web Developer, expert in: @developer.language
+//               <p class="font-italic mb-0">Lives in: <%= @developer.address %></p>
+//               <p class="font-italic mb-0"><%= @developer.user.email %></p>
+//               <p class="font-italic mb-0"><%= @developer.description
